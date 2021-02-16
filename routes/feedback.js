ad100215f8eb263b30996c1467e4a26e98b65fec
kildre <kildre@gmail.com>
2021-02-16 15:21:59 -0600

    added services, started breakingout code into components

---
+++ routes/feedback.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+module.exports = (params) => {
+
+  const { feedbackService } = params;
+  
+  router.get("/", async (req, res) => {
+    const feedback = await feedbackService.getList();
+    return res.json(feedback)
+  });
+  
+  router.post("/", (req, res) => {
+    return res.send("feedback form posted")
+  });
+
+
+
+  return router;
+}
+    
--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-import express from "express";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+const express = require("express");
+const path = require("path");
+const cookieSession = require("cookie-session");
+
+const FeedbackService = require("./services/FeedbackService");
+const SpeakerService = require("./services/SpeakerService");
+
+const feedbackService = new FeedbackService("./data/feedback.json");
+const speakersService = new SpeakerService("./data/speakers.json");
+
+const routes = require("./routes");
+
+
+
 
 // eslint-disable-next-line no-param-reassign, no-underscore-dangle
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 // const __dirname = fileURLToPath(import.meta.url);
 // const _dirname = path.dirname(_filename);
 const app = express();
 const port = 3000;
 
-app.set("views engine", "ejs");
-app.set("views", path.join("./views"));
+app.set("trust proxy", 1)
+
+app.use(
+  cookieSession({
+   name: "session",
+   keys: ["ddfer43erw", "445rresdfgw21"]
+}))
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.static(path.join(__dirname, "./static")));
 
-app.get("/", (req, res) => {
-  res.render("pages/index", {pageTitle: "Bonjour!"}
-  )
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./static/speakers.html"));
-});
+app.use("/", routes({feedbackService, speakersService}));
 
 app.listen(port, () => {
   // eslint-disable-next-line
